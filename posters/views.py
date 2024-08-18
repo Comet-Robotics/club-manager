f24c1bb3e5b131bb3773b7abf01b80ed54598558
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-08-18 06:09:57 -0500

    Added log poster to poster view

--- posters/views.py
+++ posters/views.py
@@ -2,7 +2,8 @@ from typing import Any
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from .models import Poster, Visit
+from .models import Poster, Visit, Campaign
+from .forms import PosterLogForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 
@@ -39,6 +40,25 @@ def poster_stats(request: HttpRequest) -> JsonResponse:
 @login_required
 def show_poster_stats(request):
     return render(request, 'poster_stats.html')
+
+def log_poster(request):
+    if request.method == 'POST':
+        form = PosterLogForm(request.POST)
+        if form.is_valid():
+            id = form.cleaned_data['poster_id']
+            longitude = form.cleaned_data['longitude']
+            latitude = form.cleaned_data['latitude']
+            description = form.cleaned_data['description']
+            date = timezone.now
+            campaign = Campaign.objects.get(pk=1)
+            poster = Poster.objects.get_or_create(pk=id, longitude=longitude,
+                                                  latitude=latitude, location=description,
+                                                  pub_date=date, campaign=campaign)
+            form.poster_id += 1
+            return render(request, 'poster_log.html', {'form': form})
+    else:
+        form = PosterLogForm()
+    return render(request, 'poster_log.html', {'form': form})
 
 class PosterListView(ListView):
     model = Poster
