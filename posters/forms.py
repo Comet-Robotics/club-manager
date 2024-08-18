51cd419b936d6f283a03e40254469873978c554d
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-08-18 05:34:08 -0500

    Added forms.py

---
+++ posters/forms.py
@@ -0,0 +1,8 @@
+from typing import Any
+from django import forms
+
+class PosterLogForm(forms.form):
+    poster_id = forms.IntegerField(label='Poster ID')
+    latitude = forms.FloatField(label='Latitude')
+    longitude = forms.FloatField(label='Longitude')
+    description = forms.CharField(label='Description', required=False)
