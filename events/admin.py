a1c97870a63e32378a5f917378c2c7776892092e
Mason Thomas <Kandleseatwicks@gmail.com>
2024-08-18 22:01:41 -0500

    add more to admin page

--- events/admin.py
+++ events/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 # Register your models here.
-from .models import UserIdentification, Attendance, Event
+from .models import UserIdentification, Attendance, Event, Reservation
 
 class UserIDInline(admin.StackedInline):
     model = UserIdentification
@@ -17,5 +17,6 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(UserIdentification)
 admin.site.register(Attendance)
+admin.site.register(Reservation)
 admin.site.register(Event)
 
--- posters/admin.py
+++ posters/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Campaign, Poster
+from .models import Campaign, Poster, Visit
 
 admin.site.register(Campaign)
 admin.site.register(Poster)
+admin.site.register(Visit)
