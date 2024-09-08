618befbc5e51574a23b3b23c4467db1049edd7b4
Jason Antwi-Appah <hey@jasonaa.me>
2024-09-08 17:03:00 -0500

    Fixes to payment sign in page

--- payments/forms.py
+++ payments/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from common.forms import NetIDField
 
 class PaymentSignInForm(forms.Form):
-    username = NetIDField(label='netID', max_length=20, widget=forms.TextInput(attrs={'autofocus': True}))
+    username = NetIDField(label='Net ID', max_length=20, widget=forms.TextInput(attrs={'autofocus': True}))
