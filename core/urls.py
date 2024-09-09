7431cfdee3163a38be7d4853928ea6a51cd4b907
Mason Thomas <Kandleseatwicks@gmail.com>
2024-09-08 20:05:57 -0500

    fix apple merchant id url

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
-    path('.well-known/apple-developer-merchantid', views.apple_merchant_id, name='apple_merchant_id'),
+    path('.well-known/apple-developer-merchantid-domain-association', views.apple_merchant_id, name='apple_merchant_id'),
 ]
