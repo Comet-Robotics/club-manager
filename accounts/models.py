d83f65e190ae565450b9e918be1245e688f3cf7c
Colin Wong <cwongmath@gmail.com>
2024-09-10 14:29:23 -0500

    Add date_created to accountlink

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ class AccountLink(models.Model):
     SOCIAL_CHOICES = (('discord', 'Discord'),('other', 'Other'))
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     link_type = models.CharField(max_length=200, choices=SOCIAL_CHOICES)
     social_id = models.CharField(max_length=200)
