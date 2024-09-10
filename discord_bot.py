00635a2512a21ff2dede942528ad56f14bb34453
Colin Wong <cwongmath@gmail.com>
2024-09-10 03:30:46 -0500

    discord bot should mostly work

--- discord_bot.py
+++ discord_bot.py
@@ -7,6 +7,7 @@ import discord
 django.setup()
 
 from posters.models import Campaign
+from core.models import User, UserProfile
 
 bot = discord.Bot()
 
@@ -20,33 +21,56 @@ async def link(
     ctx: discord.ApplicationContext, 
     net_id: str
 ):
-    ctx.respond("Processing...", ephemeral=True)
+    await ctx.respond("Processing...", ephemeral=True)
+
+    author_id = ctx.author.id
 
     # TODO: Implement this lol
 
-    campaign = await sync_to_async(Campaign.objects.get)()
-    await ctx.respond(f"{campaign.campaign_name}")
-
-    net_id_is_in_db = True
-
-    if not net_id_is_in_db:
+    if not (net_id[:3].isalpha() and net_id[3:].isdigit()):
+        await ctx.respond("Invalid NetID format!")
         return
 
-    net_id_is_linked = False
+    def get_user():
+        try:
+            return User.objects.get(username=net_id)
+        except:
+            return None
+    user = await sync_to_async(get_user)()
 
-    if net_id_is_linked:
-        await ctx.respond("That Net ID is already linked to a Discord account. Ping an officer if this looks wrong.", ephemeral=True)
+    if user is None:
+        await ctx.respond("NetID not found!")
         return
 
-    discord_id_is_linked = False
-    
+    def get_profile():
+        try:
+            return UserProfile.objects.get_or_create(user=user)[0]
+        except Exception as e:
+            return None
+    profile = await sync_to_async(get_profile)()
+
+    if not profile:
+        await ctx.respond("Unhandled error: ProfileNotFound")
+        return
+
+    if profile.discord_id:
+        await ctx.respond("That Net ID is already linked to a different Discord account. Ping an officer if this looks wrong.", ephemeral=True)
+        return
+
+    def is_discord_linked():
+        try:
+            return UserProfile.objects.get(discord_id=author_id) is not None
+        except:
+            return False
+    discord_id_is_linked = await sync_to_async(is_discord_linked)()
+
     if discord_id_is_linked:
         await ctx.respond("This Discord account is already linked to a Comet Robotics account. Ping an officer if this looks wrong.", ephemeral=True)
         return
 
     email = f"{net_id}@utdallas.edu"
     await ctx.respond(f"Sending email to {email}...", ephemeral=True)
-    
+
     # TODO: send email
 
     embed = discord.Embed(
@@ -54,10 +78,15 @@ async def link(
         description=f"Check your email (`{email}`) and click the link to connect your Discord account to your Comet Robotics account.",  
         color=discord.Colour.red(),
     )
- 
+
     await ctx.respond(":tada:", embed=embed, ephemeral=True)
 
- 
+    # TODO: move linking
+    profile.discord_id = ctx.author.id
+    await sync_to_async(profile.save)()
+    await ctx.respond("linked...", ephemeral=True)
+
+
 bot.run(os.getenv("DISCORD_TOKEN"))
 
 # TODO: need a way to either start this alongside the django server, or create a separate systemd service for it
