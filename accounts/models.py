d75b2ba96e7c153c59665bd66b7e37ad6ac5d62d
Colin Wong <cwongmath@gmail.com>
2024-09-10 03:30:33 -0500

    Update models

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,8 @@ import uuid
 class AccountLink(models.Model):
     SOCIAL_CHOICES = (('discord', 'Discord'),('other', 'Other'))
 
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     link_type = models.CharField(max_length=200, choices=SOCIAL_CHOICES)
     social_id = models.CharField(max_length=200)
 
--- core/models.py
+++ core/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 # Create your models here.
 class UserProfile(models.Model):
     GENDER_CHOICES = (('M', 'Male'),('F', 'Female'),('O', 'Other'))
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
-    discord_id = models.CharField(max_length=200, blank=True)
-    
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    discord_id = models.CharField(max_length=200, null=True, blank=True, unique=True)
+
     def __str__(self):
         return self.user.first_name + '_' + self.user.last_name
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    # if created:
+    if created or not hasattr(instance, "userprofile"):
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
