c51c5485cc8e47b626e7ae013e17f4aa58a5853c
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-09-09 21:59:44 -0500

    add discord id column and allow null value

--- core/models.py
+++ core/models.py
@@ -6,7 +6,8 @@ class UserProfile(models.Model):
     GENDER_CHOICES = (('M', 'Male'),('F', 'Female'),('O', 'Other'))
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    discord_id = models.CharField(max_length=200, blank=True)
     
     def __str__(self):
         return self.user.first_name + '_' + self.user.last_name
