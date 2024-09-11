4aa27e19fa8402c1dec2256e8bf20d8a00198211
Jason Antwi-Appah <hey@jasonaa.me>
2024-09-11 17:40:49 -0500

    get user info for view

--- accounts/views.py
+++ accounts/views.py
@@ -3,22 +3,32 @@ from django.views import View
 from .models import AccountLink
 from core.models import UserProfile
 import discord
+from clubManager import settings
 
 client = discord.Client()
+logged_into_discord = False
 
 class LinkSocialView(View):
     template_name = 'link_social.html'
 
-    def get(self, request, uuid):
+    async def get(self, request, uuid):
+        global logged_into_discord
         account_link = AccountLink.objects.get(uuid=uuid)
         user = account_link.user
         link_type = account_link.link_type
         social_id = account_link.social_id
         pfp = None
+        username = None
         if link_type == 'discord':
-            #   TODO: get pfp from discord
-          pass
-        return render(request, self.template_name, {'user': user, 'link_type': link_type, 'social_id': social_id, 'pfp': pfp})
+            if not logged_into_discord:
+                await client.login(settings.DISCORD_TOKEN)
+                logged_into_discord = True
+            discord_id = int(account_link.social_id)
+            user = await client.fetch_user(discord_id) 
+            if user.avatar:
+                pfp = user.avatar.url
+            username = user.name
+        return render(request, self.template_name, {'user': user, 'link_type': link_type, 'social_id': social_id, 'pfp': pfp, 'username': username})
 
     def post(self, request, uuid):
         account_link = AccountLink.objects.get(uuid=uuid)
