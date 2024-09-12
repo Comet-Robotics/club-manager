ca3da701e9105463b59190d097db9edd1ce3dd06
Colin Wong <cwongmath@gmail.com>
2024-09-11 21:04:09 -0500

    404 when invalid uuid for account linking

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import AccountLink
 from core.models import UserProfile
 import discord
 from clubManager import settings
-from asgiref.sync import sync_to_async
 import requests
 
 client = discord.Client()
@@ -15,7 +14,7 @@ class LinkSocialView(View):
 
     def get(self, request, uuid):
         global logged_into_discord
-        account_link = AccountLink.objects.get(uuid=uuid)
+        account_link = get_object_or_404(AccountLink, uuid=uuid)
         user = account_link.user
         link_type = account_link.link_type
         social_id = account_link.social_id
@@ -31,7 +30,7 @@ class LinkSocialView(View):
         return render(request, self.template_name, {'user': user, 'link_type': link_type, 'social_id': social_id, 'pfp': pfp, 'username': username})
 
     def post(self, request, uuid):
-        account_link = AccountLink.objects.get(uuid=uuid)
+        account_link = get_object_or_404(AccountLink, uuid=uuid)
         user = account_link.user
         user_profile, created = UserProfile.objects.get_or_create(user=user)
         link_type = account_link.link_type
