f51e8151c286dc206e55bb5bebf59166f4a5dbae
Mason Thomas <Kandleseatwicks@gmail.com>
2024-09-12 01:07:42 -0500

    register user profiles into the admin page

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from core.models import UserProfile
 
+from common.admin import SearchFields
+
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = "User Profile"
 # registered in events.admin
+
+admin.site.register(UserProfile, search_fields=SearchFields.USER)
