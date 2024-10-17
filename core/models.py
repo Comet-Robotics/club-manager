684e11e2c6cc17f74d21b9513efc425ce2d4fe85
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-17 01:00:45 -0500

    feat: add `is_utd_affiliate` column

--- core/models.py
+++ core/models.py
@@ -20,6 +20,7 @@ class UserProfile(models.Model):
     gender = models.CharField(max_length=1, choices=GenderChoice.choices, null=True, blank=True)
     discord_id = models.CharField(max_length=200, null=True, blank=True, unique=True)
     major = models.CharField(choices=list(get_majors().items())+[("unknown", "Unknown")], null=True, blank=True, default=None)  # NULL indicates major needs to be fetched
+    is_utd_affiliate = models.BooleanField(null=False, blank=False)
 
     def __str__(self):
         return self.user.first_name + '_' + self.user.last_name
