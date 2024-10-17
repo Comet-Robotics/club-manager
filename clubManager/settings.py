74f65d1cffc135f498f1478bdc73eca0c87812f0
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-16 23:44:14 -0500

    chore: add static root to settings.py

--- clubManager/settings.py
+++ clubManager/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.getenv('STATIC_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
