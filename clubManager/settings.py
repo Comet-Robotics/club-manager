a3f6e6d60b0c69ba1e8c75fc9c57f46861358b74
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-17 04:35:42 -0500

    chore: move debug config to env var

--- clubManager/settings.py
+++ clubManager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
 CSRF_COOKIE_SAMESITE = 'Strict'
 SESSION_COOKIE_SAMESITE = 'Strict'
