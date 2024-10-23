95790004a024fe8110bd3fb37b110bef4757c900
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-23 11:17:40 -0500

    fix: debug env evaluation

--- clubManager/settings.py
+++ clubManager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = bool(int(os.getenv('DEBUG', '0')))
 
 CSRF_COOKIE_SAMESITE = 'Strict'
 SESSION_COOKIE_SAMESITE = 'Strict'
