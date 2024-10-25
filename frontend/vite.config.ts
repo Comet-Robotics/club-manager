856b2e22074071ccf5e34f331298da77885ca49d
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-25 00:10:41 -0500

    fix: remove unused var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig((state) => ({
+export default defineConfig(() => ({
   plugins: [
     TanStackRouterVite(),
     react()
