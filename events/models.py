fb39e98c121615fded49ec84e1f78a9936ccd6bb
Jason Antwi-Appah <hey@jasonaa.me>
2024-11-08 03:59:05 -0600

    feat: add models for combat event management

--- events/models.py
+++ events/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from payments.models import Product
+from payments.models import Product, PurchasedProduct
+
 
 # Create your models here.
 class Event(models.Model):
@@ -21,32 +22,57 @@ class Attendance(models.Model):
 
 
 class CombatEvent(models.Model):
-    # SUGGESTION (to consider when I am not sleep deprived): instead of creating a CombatEvent model, add models for EventWith Waivers, EventWithPayments, EventWithRobotCombatEvents, etc.
-
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='combat_events')
+    event = models.OneToOneField(Event, on_delete=models.CASCADE, related_name='combat_event')
+    
     # Documenso is an open source DocuSign clone that we use to store the waivers for minors and adults. It sends an email with a link to the document that they can e-sign (or we can optionally embed that same flow into our project via a React component).
     # If a user's DOB indicates they are under 18, they will receive the minor waiver to sign, which also requires their parent to sign before Documenso will mark it as complete. most of what we found on COPPA said that we should be fine storing minor data as long as we have parental permission which this can cover
-    documenso_minor_waiver_id = models.CharField()
-    documenso_adult_waiver_id = models.CharField()
+    
     robot_combat_events_event_id = models.CharField()
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.event)
         
-class CombatEventRegistration(models.Model):
-    combat_event = models.ForeignKey(CombatEvent, on_delete=models.CASCADE)
-    robot_combat_events_robot_id = models.CharField()
+        
+class Waiver(models.Model):
+      event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='waivers')
+      name = models.CharField(max_length=200)
+      internal_description = models.CharField(max_length=200)
+      alternate_for_waiver_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+      minors_must_complete =  models.BooleanField(default=False)
+      documenso_id = models.CharField(max_length=200)
+      
+      def __str__(self):
+          return self.name
+          
+class CompletedWaiver(models.Model):
+    waiver = models.ForeignKey(Waiver, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
     
-    # TODO: add other RCE fields (robot name, etc)
+    # TODO: how to model a completed waiver? completed waiver could be from documenso, or a paper waiver, or a PDF?
+    # to simplify, maybe we scan in paper waivers so we either have a documenso id or a path to a PDF (in an S3 bucket?? or on local disk??? idk django had some nice stuff for this that colin or mason played with)
     
-    # TODO figure out payments. this is more complex than it seems since a user can register multiple robots
-    # but right now the payment models make a 1:1 association between a payment and a product, so 
-    # a user would have to go through the payment process multiple times to register multiple robots.
-    # We can solve this in 2 ways: add a quantity field to the payment model (easiest but least flexible) or
-    # change the payment models (a lot). A payment would no longer be directly tied to a single product ID. A payment now be associated with another table PaymentProducts that has a foreign key to the Product table and a quantity field. The Payment amount_cents would be the sum of the product prices for all the products in the PaymentProducts table and whatever service fee Square adds.
-    # 
-    # The migration for this would be annoying but doable. This is definitely a better solution but not my focus anymore so committing this rn and switching focus to forms that allow non-UTD users to pay for stuff.
+    def __str__(self):
+        return str(self.waiver) + ' - ' + str(self.user)
+    
+
+class CombatTeam(models.Model):
+  robot_combat_events_team_id = models.CharField()
+  managers = models.ManyToManyField(User, related_name='managed_teams')
+  name = models.CharField(max_length=200)
+  
+  def __str__(self):
+      return self.name
+      
+class CombatRobot(models.Model):
+  robot_combat_events_robot_id = models.CharField()
+  combat_team = models.ForeignKey(CombatTeam, on_delete=models.CASCADE)
+  purchased_product = models.ForeignKey(PurchasedProduct, on_delete=models.CASCADE)
+  
+  def __str__(self):
+      return self.name
+  
 
 class UserIdentification(models.Model):
     # TODO: we should combine the UserProfile and UserIdentification models. this model is redundant since it only has one field
