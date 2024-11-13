3da1eac19f559e52801fee5bd485b5c430302960
Jason Antwi-Appah <hey@jasonaa.me>
2024-11-13 00:14:15 -0600

    refactor: use dataclasses in RCE parser

--- common/robot_combat_events.py
+++ common/robot_combat_events.py
@@ -1,69 +1,103 @@
+from dataclasses import dataclass
+from typing import Dict, List
 import requests
 import bs4
+from typing import NamedTuple
 
+@dataclass
+class Robot:
+    name: str
+    status: str
+    img_url: str
+    rce_resource_id: str
+    rce_team_id: str
+    
 
-def get_robot_combat_event(rce_event_id):
-  r = requests.get(f"https://www.robotcombatevents.com/events/{rce_event_id}")
-  soup = bs4.BeautifulSoup(r.text, 'html.parser')
-  
-  event_title_parent_div = soup.find("div", class_='event-title')
-  assert event_title_parent_div is not None
-  event_title = event_title_parent_div.text.strip()
-  
-  event_description_parent_div = soup.find("div", class_='event-description')
-  event_description = event_description_parent_div.text.strip()
-  
-  event_details_parent_div = soup.find("div", class_='event-details')
-  event_details = {}
-  for row in event_details_parent_div.findAll('h4'):
-    row_text = row.text.strip()
-    colon_index = row_text.find(':')
-    if colon_index != -1:
-      key = row_text[:colon_index].strip()
-      value = row_text[colon_index+1:].strip()
-      event_details[key] = value
-    
-  
-  weight_class_list_parent_div = soup.find('div', class_='event-comp-table')
-  weight_class_urls = {}
-  for row in weight_class_list_parent_div.findAll('a')[0:]:
-    weight_class_urls[row.text.strip()] = 'https://www.robotcombatevents.com' + row['href']
-  
-  robots_by_weight_class = {}
-  for key, value in weight_class_urls.items():
-    robots_by_weight_class[key] = []
-    r = requests.get(value)
-    thing = bs4.BeautifulSoup(r.text, 'html.parser')
-    
-    registration_panel_div = thing.find("div", class_="registrations-panel")
-    assert registration_panel_div is not None
-    
-    registration_panel_table_rows = registration_panel_div.find_all("tr")
-    header_row = registration_panel_table_rows[0]
-    keys = [th.text.strip() for th in header_row.find_all("th")]
-    assert keys == ["", "Bot", "Team", "Status"]
-    
-    for row in registration_panel_table_rows[1:]:
-      robot = {
-        "img_url": row.find("img")["src"],
-      }
+class Team(NamedTuple):
+    name: str
+    rce_team_id: str
 
-      for i, item in enumerate([list(col)[0] for col in row.find_all("td")]):
-        if i == 0:
-          pass
-        elif i == 1:
-          robot["name"] = item.text.strip()
-          robot["rce_resource_id"] = item.attrs["href"].split("/")[-1]
-        elif i == 2:
-          robot["team"] = item.text.strip()
-          robot["rce_team_id"] = item.attrs["href"].split("/")[-1]
-        elif i == 3:
-          robot["status"] = item.text.strip()
+@dataclass
+class Event:
+    event_title: str
+    event_description: str
+    event_details: Dict[str, str]
+    robots_by_weight_class: Dict[str, List[Robot]]
+    teams: List[Team]
+
+def get_robot_combat_event(rce_event_id: str) -> Event:
+    r = requests.get(f"https://www.robotcombatevents.com/events/{rce_event_id}")
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    
+    # Get event title
+    event_title_parent_div = soup.find("div", class_='event-title')
+    assert event_title_parent_div is not None
+    event_title = event_title_parent_div.text.strip()
+    
+    # Get event description
+    event_description_parent_div = soup.find("div", class_='event-description')
+    assert event_description_parent_div is not None
+    event_description = event_description_parent_div.text.strip()
+    
+    # Get event details
+    event_details_parent_div = soup.find("div", class_='event-details')
+    assert event_details_parent_div is not None
+    event_details = {}
+    for row in event_details_parent_div.findAll('h4'):
+        row_text = row.text.strip()
+        colon_index = row_text.find(':')
+        if colon_index != -1:
+            key = row_text[:colon_index].strip()
+            value = row_text[colon_index+1:].strip()
+            event_details[key] = value
+    
+    # Get weight class URLs
+    weight_class_list_parent_div = soup.find('div', class_='event-comp-table')
+    assert weight_class_list_parent_div is not None
+    weight_class_urls = {
+        row.text.strip(): 'https://www.robotcombatevents.com' + row['href']
+        for row in weight_class_list_parent_div.findAll('a')[0:]
+    }
+    
+    # Get robots for each weight class
+    robots_by_weight_class: Dict[str, List[Robot]] = {}
+    teams: set[Team] = set()
+    for weight_class, url in weight_class_urls.items():
+        robots_by_weight_class[weight_class] = []
+        r = requests.get(url)
+        thing = bs4.BeautifulSoup(r.text, 'html.parser')
         
-      robots_by_weight_class[key].append(robot)
-      
+        registration_panel_div = thing.find("div", class_="registrations-panel")
+        assert registration_panel_div is not None
+        
+        registration_panel_table_rows = registration_panel_div.find_all("tr")
+        header_row = registration_panel_table_rows[0]
+        keys = [th.text.strip() for th in header_row.find_all("th")]
+        assert keys == ["", "Bot", "Team", "Status"]
+        
+        for row in registration_panel_table_rows[1:]:
+            cols = [list(col)[0] for col in row.find_all("td")]
+            
+            robot = Robot(
+                img_url=row.find("img")["src"],
+                name=cols[1].text.strip(),
+                rce_team_id=cols[2].attrs["href"].split("/")[-1],
+                status=cols[3].text.strip(),
+                rce_resource_id=cols[1].attrs["href"].split("/")[-1],
+            )
+            
+            teams.add(Team(
+                name=cols[2].text.strip(),
+                rce_team_id=cols[2].attrs["href"].split("/")[-1]
+            ))
+            
+            robots_by_weight_class[weight_class].append(robot)
+    
+    return Event(
+        event_title=event_title,
+        event_description=event_description,
+        event_details=event_details,
+        robots_by_weight_class=robots_by_weight_class,
+        teams=list(teams)
+    )
   
-  return {"event_title": event_title, "event_description": event_description, "event_details": event_details, "robots_by_weight_class": robots_by_weight_class}
-  
-out = get_robot_combat_event(2501)
-print(out)
