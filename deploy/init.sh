2419b3c453500c15bd4d991d591b5c3bc8382990
Jason Antwi-Appah <hey@jasonaa.me>
2025-01-22 16:27:17 -0600

    chore: update init script

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,4 +1,5 @@
-ln -s gunicorn.service /etc/systemd/system/gunicorn.service
-ln -s gunicorn.socket /etc/systemd/system/gunicorn.socket
-ln -s discord_bot.service /etc/systemd/system/discord_bot.service
-ln -s clubManager.nginx.conf /etc/nginx/sites-available/clubManager
+BASE_PATH=$(dirname "$0")
+ln -s $BASE_PATH/gunicorn.service /etc/systemd/system/gunicorn.service
+ln -s $BASE_PATH/gunicorn.socket /etc/systemd/system/gunicorn.socket
+ln -s $BASE_PATH/discord_bot.service /etc/systemd/system/discord_bot.service
+ln -s $BASE_PATH/clubManager.nginx.conf /etc/nginx/sites-available/clubManager
