6b59ca82145e4c69680587c40bfbc6663de53058
Jason Antwi-Appah <hey@jasonaa.me>
2025-01-22 23:34:41 -0600

    chore: update deploy scriopts for new setup

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,4 +1,5 @@
 BASE_PATH=$(dirname "$0")
+echo BASE_PATH=$BASE_PATH
 ln -s $BASE_PATH/gunicorn.service /etc/systemd/system/gunicorn.service
 ln -s $BASE_PATH/gunicorn.socket /etc/systemd/system/gunicorn.socket
 ln -s $BASE_PATH/discord_bot.service /etc/systemd/system/discord_bot.service
