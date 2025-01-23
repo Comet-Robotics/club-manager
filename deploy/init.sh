8dca164230af8a04427b8a91944aae4f9b77645b
Jason Antwi-Appah <hey@jasonaa.me>
2025-01-22 23:38:38 -0600

    smh my head

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,4 +1,4 @@
-script_path=$(realpath “${BASH_SOURCE:-$0}”)
+script_path=$(realpath "${BASH_SOURCE:-$0}")
 echo "The absolute path is $script_path"
 BASE_PATH=$(dirname $script_path)
 echo "The BASE_PATH is $BASE_PATH"
