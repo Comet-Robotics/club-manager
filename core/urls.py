bdd9525f6a9bb59c04b8fdcd9804f5f579351e92
democat <cwongmath@gmail.com>
2025-02-03 20:49:02 -0600

    Fix links with underscores in them

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('.well-known/apple-developer-merchantid-domain-association', views.apple_merchant_id, name='apple_merchant_id'),
-    re_path('_/*', views.spa_view, name='spa'),
+    re_path('$_/*', views.spa_view, name='spa'),
     path('', views.index, name='index'),
 ]
