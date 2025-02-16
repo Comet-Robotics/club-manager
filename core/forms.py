274e1c709e4e4c1d221b930ae3a6e714e51f80d5
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2025-02-15 20:55:42 -0600

    updated-phone-validation

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+import re
 
 class PhoneNumberField(forms.IntegerField):
-    phone = forms.IntegerField()
-
     def validate(self, value):
         super().validate(value)
-        if not is_valid_phone_number(value):
+        if not self.is_valid_phone_number(value):
             raise forms.ValidationError("Invalid Phone Number!")
+        
+    def is_valid_phone_number(self, value):
+        return re.fullmatch(r'^\+?\d{3}-\d{3}-\d{4}$', value) is not None
 
 class ContactInfoForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100)
