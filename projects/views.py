a67c27f69d102c4635557c79bd926abfd6c5801c
Jason Antwi-Appah <hey@jasonaa.me>
2025-03-05 09:05:53 -0600

    Show all current events in defined time window

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.models import ServerSettings, User
@@ -24,12 +25,18 @@ def get_layout_data(request: HttpRequest) -> LayoutData:
     return LayoutData(user=user, settings=ServerSettings.objects.get(), accessible_projects=accessible_projects)
 
 # Create your views here.
+# Used to get all the current events in a given time window. Ex: If it is now 3pm on a Monday, and the delta is set to 4 hours, we should see all events starting from 11am (3pm - 4 hours) and 7pm (3pm + 4 hours.)
+CURRENT_EVENT_WINDOW = timedelta(hours=4)
+
 @login_required
 def project_view(request, project_id):
     user = request.user
     layout_data = get_layout_data(request)
+    now = datetime.now()
+    lower_bound = now - CURRENT_EVENT_WINDOW
+    upper_bound = now + CURRENT_EVENT_WINDOW
     
-    current_event = Event.objects.filter(project=project_id).order_by('event_date').first()
+    current_events = Event.objects.filter(project=project_id, event_date__gt=lower_bound, event_date__lt=upper_bound)
     teams = Project.objects.get(pk=project_id).all_teams()
     
-    return render(request, 'project_home.html', {**layout_data, "current_event": current_event, "teams": teams})
+    return render(request, 'project_home.html', {**layout_data, "current_events": current_events, "teams": teams})
