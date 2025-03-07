6614164db2a40acad5f35315f557b0357b206f01
Jason Antwi-Appah <hey@jasonaa.me>
2025-03-06 21:20:26 -0600

    Hide payment models in admin if payments disabled

--- payments/admin.py
+++ payments/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from common.admin import SearchFields
 from more_admin_filters import MultiSelectDropdownFilter
+from clubManager import settings
 
 # Register your models here.
 
@@ -18,8 +19,8 @@ class PaymentAdmin(admin.ModelAdmin):
         ("method", MultiSelectDropdownFilter),
     ]
 
-
-admin.site.register(Product, search_fields=["name"])
-admin.site.register(Term, search_fields=["name"] + SearchFields.PRODUCT)
-admin.site.register(Payment, PaymentAdmin, search_fields=SearchFields.USER + SearchFields.PURCHASED_PRODUCT)
-admin.site.register(PurchasedProduct, search_fields=SearchFields.PRODUCT)
+if settings.ENABLE_PAYMENTS:
+    admin.site.register(Product, search_fields=["name"])
+    admin.site.register(Term, search_fields=["name"] + SearchFields.PRODUCT)
+    admin.site.register(Payment, PaymentAdmin, search_fields=SearchFields.USER + SearchFields.PURCHASED_PRODUCT)
+    admin.site.register(PurchasedProduct, search_fields=SearchFields.PRODUCT)
