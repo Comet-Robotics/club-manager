067d24bc99bd351b16d3691689370122d368a55e
democat <cwongmath@gmail.com>
2025-09-19 21:09:18 -0500

    lint

--- discord_bot.py
+++ discord_bot.py
@@ -38,9 +38,7 @@ import logging
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
-handler.setFormatter(
-    logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s")
-)
+handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s"))
 logger.addHandler(handler)
 
 LIST = [
