befda6ac372f4e64424a49e21643585be21baafe
democat <cwongmath@gmail.com>
2026-01-19 21:18:05 -0600

    Reduce discord bot logs

--- discord_bot.py
+++ discord_bot.py
@@ -36,7 +36,7 @@ import uvicorn
 import logging
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.DEBUG if settings.DEBUG else logging.INFO)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
 handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s"))
 logger.addHandler(handler)
