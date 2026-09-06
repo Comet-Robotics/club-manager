d0389ab10f3e2955417e116846c1bbe8b2ba082d
Jason Antwi-Appah <hey@jasonaa.me>
2026-09-05 17:18:16 -0700

    feat(accounts): add initial registration request page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import SetPasswordForm
+from common.forms import NetIDField
+
+
+class RegistrationRequestForm(forms.Form):
+    net_id = NetIDField(label="Net ID", max_length=20, widget=forms.TextInput(attrs={"autofocus": True}))
 
 
 class RegistrationCompletionForm(SetPasswordForm):
